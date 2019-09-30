853669fa0edb12967ee388f822a3994d86c4aaaf
JTablas <Jason.Tablas@gompersstudent.org>
2019-09-30 14:32:43 -0700

    September 30th

--- src/_03_More_Array_Fun/MoreArrayFun.java
+++ src/_03_More_Array_Fun/MoreArrayFun.java
@@ -4,23 +4,55 @@ import java.util.Random;
 
 public class MoreArrayFun {
 	//1. Create a main method to test the other methods you write.
-	
+	public static void main(String[] args) {
+	stringed();
+	back();
+	other();
+	rando();
+	}
 	
 	
 	//2. Write a method that takes an array of Strings and prints all the Strings in the array.
-	
+	static void stringed(){
+		String [] string = {"one","two","three"};
+		for(int i =0;i<string.length;i++){
+			System.out.println(string[i]);
+		}
+	}
 	
 	
 	//3. Write a method that takes an array of Strings and prints all the Strings in the array
 	//   in reverse order.
 	
+	 static void back(){
+		String[]bac= {"one","two","three"};
+		for(int o=bac.length;o>0;o--){
+			System.out.println(bac[o-1]);
+		}
+	}
+	
 	
 	
 	//4. Write a method that takes an array of Strings and prints every other String in the array.
-	
+	static void other(){
+		String[]oth={"one","two","three","four"};
+		for(int p=0;p<oth.length;p++){
+			if(p%2==0){
+			System.out.println(oth[p]);
+		}
+		}
+	}
 	
 	//5. Write a method that takes an array of Strings and prints all the Strings in the array
 	//   in a completely random order. Almost every run of the program should result in a different order.
+	
+	static void rando(){
+		String[] ra={"one","two","three","four"};
+		Random ran = new Random();
+		for(int u = 0;u<ra.length;u++){
+			System.out.println(ran.nextInt(u));
+		}
+	}
 	
 	
 }
